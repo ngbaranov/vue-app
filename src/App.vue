f2ffd6ed00ef6f200eb22991c880db2b1e2d87ae
nikolai <ngbaranov@gmail.com>
2025-10-05 08:03:26 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {ref} from "vue";
+const currentDate = ref(new Date().toLocaleDateString());
+</script>
+
+<template>
+<div>Текущая дата: {{currentDate}}</div>
+</template>
+
+<style scoped>
+
+</style>
